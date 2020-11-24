6f839a19d51c744a33daac13d7791955743337ec
msajadi824 <msajadi824@hotmail.com>
2020-11-24 07:59:23 +0330

    - اصلاح جزئی

--- DependencyInjection/PouyaSoftSDateExtension.php
+++ DependencyInjection/PouyaSoftSDateExtension.php
@@ -24,5 +24,9 @@ class PouyaSoftSDateExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $this->addAnnotatedClassesToCompile([
+            'PouyaSoft\\AppzaBundle\\Services\\',
+        ]);
     }
 }
