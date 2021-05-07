a782f95b234b69058495dee12c6e1b599ad87079
msajadi824 <msajadi824@hotmail.com>
2021-05-07 10:18:30 +0100

    - بروزرسانی سلکتایز - افزودن نوع فرم تایپ TextSelectizeType

---
+++ Form/Type/TextSelectizeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PouyaSoft\AppzaBundle\Form\Type;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TextSelectizeType extends AbstractType
+{
+    /** @var  EntityManagerInterface */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $items = $options['items'];
+        $items = is_callable($items) ? $items($this->em) : $items;
+        $view->vars['items'] = array_map(function ($item) {return ['item' => $item];}, $items);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'items' => [],
+        ]);
+
+        $resolver->setAllowedTypes('items', ['array', 'callable']);
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'text_selectize';
+    }
+}
